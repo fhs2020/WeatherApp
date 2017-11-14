844fcc978bba77c951dbba16eb35de7464ce2652
Flavio H Sousa <FlavioH007@gmail.com>
2017-11-14 11:50:05 -0200

    Add project files.

---
+++ ClientApp/app/components/weather/weather.component.ts
@@ -0,0 +1,27 @@
+﻿import { Component, Inject } from '@angular/core';
+import { Http } from '@angular/http';
+import { InjectionToken } from '@angular/core';
+ 
+
+@Component({
+    selector: 'weather',
+    templateUrl: './weather.component.html'
+})
+export class WeatherComponent {
+    public weather: Weather;
+
+    constructor(private http: Http) {
+    }
+
+    public getWeather(chosenCity: string) {
+        this.http.get('/api/weather/city/' + chosenCity).subscribe(result => {
+            this.weather = result.json();
+        });
+    }
+}
+
+interface Weather {
+    temp: string;
+    summary: string;
+    city: string;
+}
